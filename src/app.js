8ced8bde195f8481a92ddea8aa53580340060558
Talisson <talissonlux@outlook.com>
2020-09-10 03:01:05 -0300

    Mostrar informações na tela

--- src/app.js
+++ src/app.js
@@ -4,11 +4,23 @@ const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
+// Inserir informações na tela
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song => `
+    <li class="song">
+        <spang class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+    </li>
+    `).join('') // join() -> retorna uma nova string com todos os itens do array concatenados e separados por vírgulas
+}
 
 // Fazer requisição
 const fetchSongs = async term => {
     const response = await fetch(`${apiURL}/suggest/${term}`)
     const data = await response.json()
+
+    // Inserir informações na tela
+    insertSongsIntoPage(data)
 
 }
 
