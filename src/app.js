1e6ebc17e8dc0d3302f6a42152fd5de70dd52c58
Talisson <talissonlux@outlook.com>
2020-09-14 17:39:01 -0300

    Requisição para letra e prev and next

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
+
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await response.json()
+    console.log(data)
+    
+    insertSongsIntoPage(data)
+}
+
 // Inserir informações na tela
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => `
@@ -12,6 +21,16 @@ const insertSongsIntoPage = songsInfo => {
         <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
     </li>
     `).join('') // join() -> retorna uma nova string com todos os itens do array concatenados e separados por vírgulas
+
+    // Verificação para checar se o objeto songsInfo tem uma propriedade Next ou Prev
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+    return
+    }
+    prevAndNextContainer.innerHTML = ''
 }
 
 // Fazer requisição
@@ -37,4 +56,25 @@ form.addEventListener('submit', event => {
     }
 
     fetchSongs(searchTerm)
+})
+
+// Requisição para buscar a letra
+const fetchLyrics = async (artist, songTitle) => {
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+    const data = await response.json()
+
+    console.log(data)
+}
+
+// Evento para ouvir os clicks na página
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+    // Verificar se o nome da tag que foi clicada é um button
+    if (clickedElement.tagName === 'BUTTON') {
+        // Obter os valores dos atributos data-artist e data-song-title
+        const artist = clickedElement.getAttribute('data-artist')
+        const songTitle = clickedElement.getAttribute('data-song-title')
+
+        fetchLyrics(artist, songTitle)
+    }
 })
